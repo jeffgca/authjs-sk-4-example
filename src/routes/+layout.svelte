e655a3d359ed5e58d41d63fc3b4dbc39832c4fd4
Jeff Griffiths <jeff@eko-recordings.ca>
2025-05-16 10:23:18 -0700

    ran the svelte 5 migration script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import Header from "$components/header.svelte"
   import Footer from "$components/footer.svelte"
+  interface Props {
+    children?: import('svelte').Snippet;
+  }
+
+  let { children }: Props = $props();
 </script>
 
 <div class="container">
   <Header />
-  <slot />
+  {@render children?.()}
   <Footer />
 </div>
 

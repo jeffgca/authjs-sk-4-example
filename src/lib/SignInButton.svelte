19db97616fb675c15376b47bf39240295da816db
Jeff Griffiths <jeff@eko-recordings.ca>
2025-05-17 11:15:35 -0700

    small refinements

--- src/lib/SignInButton.svelte
+++ src/lib/SignInButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   interface Props {
     provider: any;
-    children?: import('svelte').Snippet;
+    children?: import("svelte").Snippet;
   }
 
   let { provider, children }: Props = $props();
